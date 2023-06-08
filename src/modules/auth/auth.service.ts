b15bf8b4b549e00a20be7c1116b06994df64de74
David200197 <david.alfonso999999@gmail.com>
2023-06-07 23:15:17 -0500

    fix: error name

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -8,10 +8,7 @@ import { UsersService } from '@/modules/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
 import { UserLogged } from '../../common/interfaces/user-logged.interface';
-import {
-  INVALID_CREDENTIALS,
-  USER_IS_UNAUTHORIZED,
-} from '@/common/messages/errors';
+import { INVALID_CREDENTIALS } from '@/common/messages/errors';
 import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
@@ -34,7 +31,7 @@ export class AuthService {
       throw error;
     });
     const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) throw new UnauthorizedException(USER_IS_UNAUTHORIZED);
+    if (!isValid) throw new UnauthorizedException(INVALID_CREDENTIALS);
 
     const permissions =
       user?.rol?.rolesOnPermissions?.map(({ permission }) => permission.name) ||
